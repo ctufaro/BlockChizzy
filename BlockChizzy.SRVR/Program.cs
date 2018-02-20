b6fa28c579e62a9180180aae40f999e6d7543264
unknown <ctufaro@gmail.com>
2018-02-20 13:50:46 -0500

    Adding simple server

---
+++ BlockChizzy.SRVR/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace BlockChizzy.SRVR
+{
+    //http://www.technical-recipes.com/2016/creating-a-web-server-in-c/
+    public class WebServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+
+        public WebServer(IReadOnlyCollection<string> prefixes, Func<HttpListenerRequest, string> method)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
+            }
+
+            // URI prefixes are required eg: "http://localhost:8080/test/"
+            if (prefixes == null || prefixes.Count == 0)
+            {
+                throw new ArgumentException("URI prefixes are required");
+            }
+
+            if (method == null)
+            {
+                throw new ArgumentException("responder method required");
+            }
+
+            foreach (var s in prefixes)
+            {
+                _listener.Prefixes.Add(s);
+            }
+
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
+            : this(prefixes, method)
+        {
+        }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem(c =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                if (ctx == null)
+                                {
+                                    return;
+                                }
+
+                                var rstr = _responderMethod(ctx.Request);
+                                var buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch
+                            {
+                                // ignored
+                            }
+                            finally
+                            {
+                                // always close the stream
+                                if (ctx != null)
+                                {
+                                    ctx.Response.OutputStream.Close();
+                                }
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // ignored
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+    }
+
+    internal class Program
+    {
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            if (request.HttpMethod.Equals("POST"))
+            {
+                string val = GetRequestPostData(request).Replace("data=", "");
+                return val;
+            }
+
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+        }
+
+        public static string GetRequestPostData(HttpListenerRequest request)
+        {
+            if (!request.HasEntityBody)
+            {
+                return null;
+            }
+            using (System.IO.Stream body = request.InputStream) // here we have data
+            {
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(body))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            var ws = new WebServer(SendResponse, "http://localhost:8080/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+    }
+}
